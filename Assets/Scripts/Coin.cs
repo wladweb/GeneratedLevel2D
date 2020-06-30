d791bdb9e64659a77c728d0189fe502219da1521
wladweb <wladwebwork@gmail.com>
2020-06-30 16:42:43 +0300

    coin collecting sound added

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Coin : MonoBehaviour
+{
+    private CoinsCollectSound _collectSound;
+
+    public event UnityAction CoinCollected;
+
+    private void OnEnable()
+    {
+        _collectSound = FindObjectOfType<CoinsCollectSound>();
+        CoinCollected += _collectSound.OnCoinCollected;
+    }
+
+    private void OnDisable()
+    {
+        CoinCollected -= _collectSound.OnCoinCollected;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            player.AddCoin();
+            CoinCollected?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CoinsCollectSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CoinsCollectSound : MonoBehaviour
+{
+    private AudioSource _sound;
+
+    private void Start()
+    {
+        _sound = GetComponent<AudioSource>();
+    }
+
+    public void OnCoinCollected()
+    {
+        _sound.Play();
+    }
+}
