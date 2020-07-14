7d9ad7c8ca57a5092623bd2b157bba851385283c
wladweb <wladwebwork@gmail.com>
2020-07-14 20:51:31 +0300

    changed bad fieldName

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -3,11 +3,11 @@
 public class CameraMover : MonoBehaviour
 {
     [SerializeField] private Transform _player;
-    [SerializeField] private float _smoothSpeed;
+    [SerializeField] private float _smoothInterpolant;
 
     private void Update()
     {
         Vector3 targetPosition = new Vector3(_player.position.x, transform.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, targetPosition, _smoothSpeed);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, _smoothInterpolant);
     }
 }
